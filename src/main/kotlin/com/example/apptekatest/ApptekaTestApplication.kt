85e6a5ebc1e529a29e629883bf8220275ce9ecfa
Aibek Jumadillayev <aybek.dzhum@gmail.com>
2023-07-18 19:57:59 +0600

    minor changes

--- src/main/kotlin/com/example/apptekatest/ApptekaTestApplication.kt
+++ src/main/kotlin/com/example/apptekatest/ApptekaTestApplication.kt
@@ -1,7 +1,6 @@
 package com.example.apptekatest
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
